53534a780d74d756409df803cb3a0ffea9afecbf
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2025-11-06 09:05:12 -0500

    Creacion de lista par

--- pruebaingreso.py
+++ pruebaingreso.py
@@ -2,4 +2,13 @@
 
 #Creacion de una lista con números del 1 al 100
 numeros = list(range(1, 101))
-print(numeros)
+# print(numeros)
+
+#Lista de los numeros pares
+num_pares = []
+
+for n in numeros:
+    if n % 2 == 0:
+        num_pares.append(n)
+        
+print("Numeros pares:", num_pares)
