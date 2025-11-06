e7d1cafd60fb8d3285c939c7c25b577a4997caee
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2025-11-06 09:14:10 -0500

    Creacion del diccionario

--- pruebaingreso.py
+++ pruebaingreso.py
@@ -1,8 +1,8 @@
 #Manipulación de listas y diccionarios
 
-#Creacion de una lista con números del 1 al 100
-numeros = list(range(1, 101))
-# print(numeros)
+#Creacion de una lista con números del 1 al 50
+numeros = list(range(1, 51))
+print("Lista inicial: ", numeros)
 
 #Lista de los numeros pares
 num_pares = []
@@ -10,5 +10,19 @@ num_pares = []
 for n in numeros:
     if n % 2 == 0:
         num_pares.append(n)
-        
+
 print("Numeros pares:", num_pares)
+
+#Lista con el cuadrado de cada numero
+cuadrados = []
+for n in numeros:
+    cuadrados.append(n ** 2)
+
+print("Cuadrados:", cuadrados)
+
+#Diccionario con el numero original y sus cuadrados
+diccionario = {}
+for n in numeros:
+    diccionario[n] = n ** 2
+
+print("Diccionario de numero original: cuadrados:", diccionario)
