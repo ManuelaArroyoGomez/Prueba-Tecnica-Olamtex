c52544594223923ecd7c3374e62c8e4121076827
Manuela Arroyo <manuela.arroyo@utp.edu.co>
2025-11-06 10:06:12 -0500

    Configuracion del modelo

--- notas-rapidas/app/Models/Note.php
+++ notas-rapidas/app/Models/Note.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Note extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content'
+    ];
 }
